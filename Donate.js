c456b6572b4639fcd952bbd7700a0769db8e3b8d
avikalsagar <73018081+avikalsagar@users.noreply.github.com>
2020-11-03 16:59:54 +0530

    Add files via upload

---
+++ Donate.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Donate extends Component {
+    render(){
+        return(
+            <div>
+                <h2>Donate</h2>
+                <p>
+                    Donate now on the given link <a href=" http://donate.gongo.com">forum</a>  
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Donate;
