58b0a26c3aae51b6a85ad3d9b78288003691b629
adk19800 <50418489+adk19800@users.noreply.github.com>
2021-08-09 12:05:48 +0530

    Update settings.py

--- BackEnd/dbwebdev/settings.py
+++ BackEnd/dbwebdev/settings.py
@@ -170,5 +170,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kaiajmhere@gmail.com'
-EMAIL_HOST_PASSWORD = 'mjaiak246'
+EMAIL_HOST_USER = #add your prefered email id
+EMAIL_HOST_PASSWORD = #add password
